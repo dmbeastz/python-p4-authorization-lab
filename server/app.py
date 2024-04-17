de867edff997c16b136a7c5b1cc08577aa16fe04
Dave <daveroymutisya2@gmail.com>
2024-04-17 20:28:37 +0300

    Done

--- server/app.py
+++ server/app.py
@@ -18,81 +18,80 @@ db.init_app(app)
 
 api = Api(app)
 
+
 class ClearSession(Resource):
 
     def delete(self):
-    
         session['page_views'] = None
         session['user_id'] = None
-
         return {}, 204
 
+
 class IndexArticle(Resource):
-    
+
     def get(self):
         articles = [article.to_dict() for article in Article.query.all()]
         return make_response(jsonify(articles), 200)
 
+
 class ShowArticle(Resource):
 
     def get(self, id):
-
         article = Article.query.filter(Article.id == id).first()
         article_json = article.to_dict()
-
         if not session.get('user_id'):
-            session['page_views'] = 0 if not session.get('page_views') else session.get('page_views')
+            session['page_views'] = 0 if not session.get(
+                'page_views') else session.get('page_views')
             session['page_views'] += 1
-
             if session['page_views'] <= 3:
                 return article_json, 200
-
             return {'message': 'Maximum pageview limit reached'}, 401
-
         return article_json, 200
+
 
 class Login(Resource):
 
     def post(self):
-        
         username = request.get_json().get('username')
         user = User.query.filter(User.username == username).first()
-
         if user:
-        
             session['user_id'] = user.id
             return user.to_dict(), 200
-
         return {}, 401
+
 
 class Logout(Resource):
 
     def delete(self):
-
         session['user_id'] = None
-        
         return {}, 204
+
 
 class CheckSession(Resource):
 
     def get(self):
-        
         user_id = session['user_id']
         if user_id:
             user = User.query.filter(User.id == user_id).first()
             return user.to_dict(), 200
-        
         return {}, 401
 
+
 class MemberOnlyIndex(Resource):
-    
+
     def get(self):
-        pass
+        if not session.get('user_id'):
+            return {'error': 'Unauthorized'}, 401
+        
+
 
 class MemberOnlyArticle(Resource):
-    
+
     def get(self, id):
-        pass
+        if not session.get('user_id'):
+            return {'error': 'Unauthorized'}, 401
+        
+        
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
